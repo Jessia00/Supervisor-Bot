eb33e0acc1e8759ff4139be1e1ad3ba93be9561f
Pashq <85317436+Pashq2@users.noreply.github.com>
2022-01-30 04:16:32 +0300

    Update booster.js

--- src/commands/booster.js
+++ src/commands/booster.js
@@ -1,4 +1,4 @@
-const ayar = require("../configs/config.json")
+const ayar = require("../configs/config.json");
 module.exports = {
     conf: {
       aliases: ["zengin","booster"],
@@ -7,30 +7,30 @@ module.exports = {
       description: "Boosterlerin isim değiştirmesine yardımcı olur",
     },
   
-run: async (client, message, args, embed, prefix) => {
-    let booster = ayar.registration.booster || undefined;
-    if(!booster) 
+run: async (client, message, args) => {
+    const booster = ayar.registration.booster || undefined;
+    if (!booster) 
     {
-    message.react(ayar.emojis.cross)
+    message.react(ayar.emojis.cross);
     message.channel.send("Booster değilsin!").then(x => x.delete({timeout: 5000})); 
-    return }
-    if(!message.member.roles.cache.has(booster)) 
+    return };
+    if (!message.member.roles.cache.has(booster)) 
     {
-    message.react(ayar.emojis.cross)
-    message.channel.send("Bu Komutu Kullanabilmek İçin Booster Olmalısın!").then(x=> x.delete({timeout: 5000})) 
-    return }
-    let uye = message.guild.members.cache.get(message.author.id);
-    let isim = args.filter(arg => isNaN(arg)).map(arg => arg.charAt(0).replace('i', "İ").toUpperCase()+arg.slice(1)).join(" ");
+    message.react(ayar.emojis.cross);
+    message.channel.send("Bu Komutu Kullanabilmek İçin Booster Olmalısın!").then(x=> x.delete({timeout: 5000}));
+    return };
+    const uye = message.guild.members.cache.get(message.author.id);
+    const isim = args.filter(arg => isNaN(arg)).map(arg => arg.charAt(0).replace('i', "İ").toUpperCase()+arg.slice(1)).join(" ");
     let boost;
-    if(!isim) 
+    if (!isim) 
     {
-    message.react(ayar.emojis.cross)
+    message.react(ayar.emojis.cross);
     message.channel.send("Geçerli bir isim belirtmelisin!").then(x => x.delete({timeout: 5000})); 
-    return }
-    boost = `${uye.user.username.includes(ayar.registration.tag) ? ayar.registration.tag : (ayar.registration.ikinciTag ? ayar.registration.ikinciTag : (ayar.registration.tag || ""))} ${isim}`;
-    uye.setNickname(`${boost}`).catch() 
-    message.react(ayar.emojis.mark)
-    message.channel.send(`Başarıyla ismini \`"${boost}"\` olarak değiştirdim!`).then(x => x.delete({timeout: 5000}));
+    return };
+    boostt = `${uye.user.username.includes(ayar.registration.tag) ? ayar.registration.tag : (ayar.registration.ikinciTag ? ayar.registration.ikinciTag : (ayar.registration.tag || ""))} ${isim}`;
+    uye.setNickname(`${boostt}`).catch(); 
+    message.react(ayar.emojis.mark);
+    message.channel.send(`Başarıyla ismini \`"${boostt}"\` olarak değiştirdim!`).then(x => x.delete({timeout: 5000}));
 },
   };
   
