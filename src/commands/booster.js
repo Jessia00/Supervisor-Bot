383850c7e6e12f94e0e8461d875a128ce8043d6f
Pashq <85317436+Pashq2@users.noreply.github.com>
2022-01-30 04:18:04 +0300

    Update booster.js

--- src/commands/booster.js
+++ src/commands/booster.js
@@ -13,20 +13,20 @@ run: async (client, message, args) => {
     {
     message.react(ayar.emojis.cross);
     message.channel.send("Booster değilsin!").then(x => x.delete({timeout: 5000})); 
-    return };
+    return }
     if (!message.member.roles.cache.has(booster)) 
     {
     message.react(ayar.emojis.cross);
     message.channel.send("Bu Komutu Kullanabilmek İçin Booster Olmalısın!").then(x=> x.delete({timeout: 5000}));
-    return };
+    return }
     const uye = message.guild.members.cache.get(message.author.id);
     const isim = args.filter(arg => isNaN(arg)).map(arg => arg.charAt(0).replace('i', "İ").toUpperCase()+arg.slice(1)).join(" ");
-    let boostt;
+    const boostt;
     if (!isim) 
     {
     message.react(ayar.emojis.cross);
     message.channel.send("Geçerli bir isim belirtmelisin!").then(x => x.delete({timeout: 5000})); 
-    return };
+    return }
     boostt = `${uye.user.username.includes(ayar.registration.tag) ? ayar.registration.tag : (ayar.registration.ikinciTag ? ayar.registration.ikinciTag : (ayar.registration.tag || ""))} ${isim}`;
     uye.setNickname(`${boostt}`).catch(); 
     message.react(ayar.emojis.mark);
