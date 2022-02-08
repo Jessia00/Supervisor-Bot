78ca215a6cb9a6c8c235575afe1a6dfc8d88a2e0
Pashq <85317436+Pashq2@users.noreply.github.com>
2022-02-08 07:48:52 +0300

    Update aspect.js

--- aspect.js
+++ aspect.js
@@ -1,5 +1,6 @@
 const { Client, Collection } = require("discord.js");
 const client = (global.client = new Client({ fetchAllMembers: true }));
+require('discord-buttons')(client)
 const settings = require("./src/configs/settings.json");
 const ayar = require("./src/configs/config.json");
 client.commands = new Collection();
@@ -69,3 +70,51 @@ client.on("userUpdate", async (stg, yeni,message) => {
 
 });
 
+
+ client.on("message", (message) => {
+
+    if (message.content !== "!rolalma" || message.author.bot) return;
+
+    client.api.channels(message.channel.id).messages.post({ data: {"content": `Merhaba **Valence** üyeleri,\nÇekiliş katılımcısı alarak __**Nitro**__ , __**Spotify**__ , __**Netflix**__ , __**Exxen**__ , __**BluTv**__ gibi çeşitli ödüllerin sahibi olabilirsiniz.\nEtkinlik katılımcısı alarak çeşitli etkinliklerin yapıldığı anlarda herkesten önce haberdar olabilirsiniz ve çekilişlere önceden katılma hakkı kazanabilirsiniz.\n\n__Aşağıda ki butonlara basarak siz de bu ödülleri kazanmaya hemen başlayabilirsiniz!__`,
+    "components":[{
+        "type":1,"components":[
+                                 {"type":2,"style":4,"custom_id":"cekilis","label":"🎁 Çekiliş Katılımcısı"},
+                                 {"type":2,"style":3,"custom_id":"etkinlik","label":"🎉 Etkinlik Katılımcısı"},
+               ]}    
+]}
+        
+
+
+
+         })
+
+
+        });
+
+
+client.on('clickButton', async (button) => {
+  // Button1
+    if (button.id === 'cekilis') {
+        if (button.clicker.member.roles.cache.get(ayar.Roles.cekilis)) {
+            await button.clicker.member.roles.remove(ayar.Roles.cekilis)
+            await button.reply.think(true);
+            await button.reply.edit(`Rolleriniz güncellendi.`)
+        } else {
+            await button.clicker.member.roles.add(ayar.Roles.cekilis)
+            await button.reply.think(true);
+            await button.reply.edit(`Rolleriniz güncellendi.`)
+        }
+    }
+
+    if (button.id === 'etkinlik') {
+        if (button.clicker.member.roles.cache.get(ayar.Roles.etkinlik)) {
+            await button.clicker.member.roles.remove(ayar.Roles.etkinlik)
+            await button.reply.think(true);
+            await button.reply.edit(`Rolleriniz güncellendi.`)
+        } else {
+            await button.clicker.member.roles.add(ayar.Roles.etkinlik)
+            await button.reply.think(true);
+            await button.reply.edit(`Rolleriniz güncellendi.`)
+        }
+    }
+});
